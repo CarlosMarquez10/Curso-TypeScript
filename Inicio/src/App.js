3e7d9e0b68d8e6dde7beab66279ead8606209626
Carlosmarquez27 <carlos27marquez10@gmailcom>
2024-08-09 13:44:20 -0500

    Presentacion

---
+++ Inicio/src/App.js
@@ -0,0 +1,5 @@
+"use strict";
+let Data;
+Data = "Inicio del curso con TypeScript";
+let ContRoot = document.getElementById("container-root");
+ContRoot === null || ContRoot === void 0 ? void 0 : ContRoot.innerHTML = `<h1>${Data}</h1>`;
---
+++ Inicio/src/App.ts
@@ -0,0 +1,5 @@
+let Data: string;
+Data = "Inicio del curso con TypeScript"
+let ContRoot = document.getElementById("container-root");
+ContRoot?.innerHTML = `<h1>${Data}</h1>`
+
